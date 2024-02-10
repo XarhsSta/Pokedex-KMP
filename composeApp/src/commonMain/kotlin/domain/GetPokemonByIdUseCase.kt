4e249c2a921b25679753507cbe04c2eb63303d87
XarhsSta <h.stasinos72@gmail.com>
2024-02-10 21:29:59 +0200

    Added GetPokemonByIdUseCase

---
+++ composeApp/src/commonMain/kotlin/domain/GetPokemonByIdUseCase.kt
@@ -0,0 +1,19 @@
+package domain
+
+import data.models.PokemonInfo
+import data.repository.PokemonRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+
+class GetPokemonByIdUseCase(
+    private val pokemonRepository: PokemonRepository,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO
+): UseCase<GetPokemonByIdUseCase.Params, PokemonInfo>(dispatcher) {
+
+    data class Params(val id: Int)
+
+    override suspend fun execute(parameters: Params): Result<PokemonInfo> {
+        return pokemonRepository.getPokemonById(parameters.id)
+    }
+}
