61825b260a5792ab9e7c0a4f3b0612e248689e08
XarhsSta <h.stasinos72@gmail.com>
2024-02-11 13:31:13 +0200

    Formatted PokemonType class

--- composeApp/src/commonMain/kotlin/data/models/entity/PokemonType.kt
+++ composeApp/src/commonMain/kotlin/data/models/entity/PokemonType.kt
@@ -1,24 +1,84 @@
 package data.models.entity
 
-enum class PokemonType(hexColor: String, imageUrl: String){
-    NORMAL("A8A77a", "https://archives.bulbagarden.net/media/upload/thumb/0/08/NormalIC_SV.png/70px-NormalIC_SV.png"),
-    FIGHTING("C22E28", "https://archives.bulbagarden.net/media/upload/thumb/0/0f/FightingIC_SV.png/70px-FightingIC_SV.png"),
-    FLYING("A98FF3", "https://archives.bulbagarden.net/media/upload/thumb/d/d7/FlyingIC_SV.png/70px-FlyingIC_SV.png"),
-    POISON("A33EA1", "https://archives.bulbagarden.net/media/upload/thumb/9/9d/PoisonIC_SV.png/70px-PoisonIC_SV.png"),
-    GROUND("E2BF65", "https://archives.bulbagarden.net/media/upload/thumb/f/f8/GroundIC_SV.png/70px-GroundIC_SV.png"),
-    ROCK("B6A136", "https://archives.bulbagarden.net/media/upload/thumb/3/32/RockIC_SV.png/70px-RockIC_SV.png"),
-    BUG("A6B91A", "https://archives.bulbagarden.net/media/upload/thumb/d/d1/BugIC_SV.png/70px-BugIC_SV.png"),
-    GHOST("735797", "https://archives.bulbagarden.net/media/upload/thumb/2/2c/GhostIC_SV.png/70px-GhostIC_SV.png"),
-    STEEL("B7B7CE", "https://archives.bulbagarden.net/media/upload/thumb/b/b8/SteelIC_SV.png/70px-SteelIC_SV.png"),
-    FIRE("EE8130", "https://archives.bulbagarden.net/media/upload/thumb/a/a2/FireIC_SV.png/70px-FireIC_SV.png"),
-    WATER("6390F0", "https://archives.bulbagarden.net/media/upload/thumb/d/de/WaterIC_SV.png/70px-WaterIC_SV.png"),
-    GRASS("7AC74C", "https://archives.bulbagarden.net/media/upload/thumb/7/7b/GrassIC_SV.png/70px-GrassIC_SV.png"),
-    ELECTRIC("F7D02C", "https://archives.bulbagarden.net/media/upload/thumb/7/77/ElectricIC_SV.png/70px-ElectricIC_SV.png"),
-    PSYCHIC("F95587", "https://archives.bulbagarden.net/media/upload/thumb/9/96/PsychicIC_SV.png/70px-PsychicIC_SV.png"),
-    ICE("96D9D6", "https://archives.bulbagarden.net/media/upload/thumb/1/13/IceIC_SV.png/70px-IceIC_SV.png"),
-    DRAGON("6F35FC", "https://archives.bulbagarden.net/media/upload/thumb/7/7f/DragonIC_SV.png/70px-DragonIC_SV.png"),
-    DARK("705736", "https://archives.bulbagarden.net/media/upload/thumb/3/30/DarkIC_SV.png/70px-DarkIC_SV.png"),
-    FAIRY("D685AD", "https://archives.bulbagarden.net/media/upload/thumb/c/c6/FairyIC_SV.png/70px-FairyIC_SV.png"),
-    UNKNOWN("000000", "https://archives.bulbagarden.net/media/upload/3/3c/UnknownIC_Big.png"),
-    SHADOW("000000", "https://archives.bulbagarden.net/media/upload/3/3c/UnknownIC_Big.png")
+enum class PokemonType(val hexColor: String, val imageUrl: String) {
+    NORMAL(
+        hexColor = "A8A77A",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/0/08/NormalIC_SV.png/70px-NormalIC_SV.png"
+    ),
+    FIGHTING(
+        hexColor = "C22E28",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/0/0f/FightingIC_SV.png/70px-FightingIC_SV.png"
+    ),
+    FLYING(
+        hexColor = "A98FF3",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/d/d7/FlyingIC_SV.png/70px-FlyingIC_SV.png"
+    ),
+    POISON(
+        hexColor = "A33EA1",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/9/9d/PoisonIC_SV.png/70px-PoisonIC_SV.png"
+    ),
+    GROUND(
+        hexColor = "E2BF65",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/f/f8/GroundIC_SV.png/70px-GroundIC_SV.png"
+    ),
+    ROCK(
+        hexColor = "B6A136",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/3/32/RockIC_SV.png/70px-RockIC_SV.png"
+    ),
+    BUG(
+        hexColor = "A6B91A",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/d/d1/BugIC_SV.png/70px-BugIC_SV.png"
+    ),
+    GHOST(
+        hexColor = "735797",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/2/2c/GhostIC_SV.png/70px-GhostIC_SV.png"
+    ),
+    STEEL(
+        hexColor = "B7B7CE",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/b/b8/SteelIC_SV.png/70px-SteelIC_SV.png"
+    ),
+    FIRE(
+        hexColor = "EE8130",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/a/a2/FireIC_SV.png/70px-FireIC_SV.png"
+    ),
+    WATER(
+        hexColor = "6390F0",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/d/de/WaterIC_SV.png/70px-WaterIC_SV.png"
+    ),
+    GRASS(
+        hexColor = "7AC74C",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/7/7b/GrassIC_SV.png/70px-GrassIC_SV.png"
+    ),
+    ELECTRIC(
+        hexColor = "F7D02C",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/7/77/ElectricIC_SV.png/70px-ElectricIC_SV.png"
+    ),
+    PSYCHIC(
+        hexColor = "F95587",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/9/96/PsychicIC_SV.png/70px-PsychicIC_SV.png"
+    ),
+    ICE(
+        hexColor = "96D9D6",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/1/13/IceIC_SV.png/70px-IceIC_SV.png"
+    ),
+    DRAGON(
+        hexColor = "6F35FC",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/7/7f/DragonIC_SV.png/70px-DragonIC_SV.png"
+    ),
+    DARK(
+        hexColor = "705736",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/3/30/DarkIC_SV.png/70px-DarkIC_SV.png"
+    ),
+    FAIRY(
+        hexColor = "D685AD",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/thumb/c/c6/FairyIC_SV.png/70px-FairyIC_SV.png"
+    ),
+    UNKNOWN(
+        hexColor = "000000",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/3/3c/UnknownIC_Big.png"
+    ),
+    SHADOW(
+        hexColor = "000000",
+        imageUrl = "https://archives.bulbagarden.net/media/upload/3/3c/UnknownIC_Big.png"
+    )
 }
