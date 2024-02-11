062b5bdcb2443a195085cfa7e0dcdb1ad09f7b0e
XarhsSta <h.stasinos72@gmail.com>
2024-02-11 20:12:42 +0200

    Prototyped a basic HomeView

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,24 +1,8 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import data.network.PokeApiService
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
-import ui.ViewModel
 import ui.screens.DetailedViewScreen
+import ui.screens.HomeView
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -40,6 +24,6 @@ fun App() {
 //                }
 //            }
 //        }
-        DetailedViewScreen()
+        HomeView()
     }
 }
