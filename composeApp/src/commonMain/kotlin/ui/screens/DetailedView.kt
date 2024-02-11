b8054f7df0ddf451cd26e360a3bdeb8525fa8a34
XarhsSta <h.stasinos72@gmail.com>
2024-02-11 12:57:26 +0200

    Added a prototype table for Base Stats

--- composeApp/src/commonMain/kotlin/ui/screens/DetailedView.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/DetailedView.kt
@@ -1,19 +1,28 @@
 package ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import data.models.entity.PokemonInfo
 import data.models.entity.PokemonStat
@@ -57,19 +66,60 @@ private fun PokemonImage(pokemon: PokemonInfo) {
 
 @Composable
 private fun PokemonStats(stats: List<PokemonStat>) {
-    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Base Stats", modifier = Modifier.padding(vertical = 8.dp))
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Text(
+            text = "Base Stats",
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
         stats.forEach { StatEntry(it) }
+        Text(
+            text = "Total Base Stat: ${stats.map { it.baseStat }.sum()}",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
     }
 }
 
 @Composable
 private fun StatEntry(stat: PokemonStat) {
     Row(
-        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+        modifier = Modifier.fillMaxWidth().height(IntrinsicSize.Min),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = stat.name.formattedName)
-        Text(text = stat.baseStat.toString())
+        Text(text = stat.name.formattedName, modifier = Modifier.weight(0.2f))
+        StatBar(stat.baseStat, stat.name.color)
+        Text(
+            text = stat.baseStat.toString(),
+            modifier = Modifier.weight(0.2f),
+            textAlign = TextAlign.End
+        )
+    }
+}
+
+@Composable
+private fun RowScope.StatBar(baseValue: Int, color: Color) {
+    val maxBaseStat = 255
+    val widthToFill = baseValue.toDouble() / maxBaseStat
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .weight(1f)
+    ) {
+        Spacer(
+            modifier = Modifier
+                .clip(
+                    RoundedCornerShape(10.dp)
+                )
+                .fillMaxWidth(widthToFill.toFloat())
+                .fillMaxHeight()
+                .background(color)
+        )
     }
 }
