7009a727dc50f1cd91f0bfbd77668678cc047240
XarhsSta <h.stasinos72@gmail.com>
2024-02-11 13:31:42 +0200

    Added GradientBackground in PokemonImage

--- composeApp/src/commonMain/kotlin/ui/screens/DetailedView.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/DetailedView.kt
@@ -21,6 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -30,6 +33,12 @@ import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 import ui.ViewModel
 import util.capitalize
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 @Composable
 fun DetailedViewScreen() {
@@ -48,7 +57,14 @@ fun DetailedViewScreen() {
 
 @Composable
 private fun PokemonImage(pokemon: PokemonInfo) {
-    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .gradientBackground(
+                colors = pokemonGradientColors(pokemon),
+                angle = 45f
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         if (pokemon.isNotEmpty()) {
             KamelImage(
                 resource = asyncPainterResource(data = pokemon.sprite.defaultSprite),
@@ -58,7 +74,11 @@ private fun PokemonImage(pokemon: PokemonInfo) {
         }
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             pokemon.type.forEach {
-                Text(text = it.name, modifier = Modifier.padding(horizontal = 8.dp))
+                KamelImage(
+                    resource = asyncPainterResource(data = it.imageUrl),
+                    contentDescription = "Pokemon Type",
+                    modifier = Modifier.padding(horizontal = 8.dp).size(96.dp)
+                )
             }
         }
     }
@@ -123,3 +143,45 @@ private fun RowScope.StatBar(baseValue: Int, color: Color) {
         )
     }
 }
+
+private fun pokemonGradientColors(pokemon: PokemonInfo): List<Color> {
+    return if (pokemon.type.size == 1) {
+        listOf(
+            pokemon.type[0].hexColor.toColor(),
+            Color.White
+        )
+    } else {
+        pokemon.type.map { it.hexColor.toColor() }
+    }
+}
+
+private fun String.toColor(): Color {
+    val rgb = this.chunked(2)
+    return Color(red = rgb[0].toInt(16), green = rgb[1].toInt(16), blue = rgb[2].toInt(16))
+}
+
+@Composable
+fun Modifier.gradientBackground(colors: List<Color>, angle: Float) = this.then(
+    Modifier.drawBehind {
+        val angleRad = angle / 180f * PI
+        val x = cos(angleRad).toFloat() //Fractional x
+        val y = sin(angleRad).toFloat() //Fractional y
+
+        val radius = sqrt(size.width.pow(2) + size.height.pow(2)) / 2f
+        val offset = center + Offset(x * radius, y * radius)
+
+        val exactOffset = Offset(
+            x = min(offset.x.coerceAtLeast(0f), size.width),
+            y = size.height - min(offset.y.coerceAtLeast(0f), size.height)
+        )
+
+        drawRect(
+            brush = Brush.linearGradient(
+                colors = colors,
+                start = Offset(size.width, size.height) - exactOffset,
+                end = exactOffset
+            ),
+            size = size
+        )
+    }
+)
