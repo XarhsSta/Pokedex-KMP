3befd4e86dd07a73417c65b105d637e9e60f3da8
XarhsSta <h.stasinos72@gmail.com>
2024-03-16 13:14:13 +0200

    Code cleanup

--- composeApp/src/commonMain/kotlin/ui/viewmodel/ViewModel.kt
+++ composeApp/src/commonMain/kotlin/ui/viewmodel/ViewModel.kt
@@ -1,31 +1,18 @@
 package ui.viewmodel
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.paging.PagingData
-import co.touchlab.kermit.Logger
 import data.models.entity.PagedPokemonInfo
 import data.models.entity.PokemonInfo
-import data.repository.MovesRepository
 import data.repository.PokemonRepository
-import data.Result
 import domain.GetPokemonByIdUseCase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
 import util.RequestState
 import util.RequestState.Idle.requestState
-import util.Resource
-import util.resource
 
 class ViewModel(
     val getPokemonByIdUseCase: GetPokemonByIdUseCase,
-    val pokemonRepository: PokemonRepository,
-    val movesRepository: MovesRepository
+    val pokemonRepository: PokemonRepository
 ) {
     val pokemonFlow = getPokemon()
 
